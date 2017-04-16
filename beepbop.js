a93d935dbfee478d8f201d09e3a5f5678c2f7768
Scott Krawczyk <skrawczyk1000@gmail.com>
2017-04-16 01:07:51 -0400

    Update beepbop.js

--- beepbop.js
+++ beepbop.js
@@ -6,12 +6,12 @@ var p1Score = 0;
 var p2Score = 0;
 var w = $('#ball').width();
 var h;
-var centerL = ($("#field").width()/2) - (w/2);
+var centerL = ($("#field").width()/2) - (w/4);
 var centerT;
 $(document).ready(function(){
 	$('#ball').height(w);
 	h = $('#ball').height();
-	centerT = ($("#field").height()/2) - (h/2);
+	centerT = ($("#field").height()/2) - (h/4);
  	$("#ball").css("top", centerT);
 	$("#ball").css("left", centerL);
 	$("#p1").html(p1Score);
