603a135274d63c228e853a21a9b0f7773e1303f1
Scott Krawczyk <skrawczyk1000@gmail.com>
2017-04-24 13:36:47 -0400

    Update beepbop.js

--- beepbop.js
+++ beepbop.js
@@ -34,13 +34,15 @@ function game(){
 
 	// create a renderer
 	var render = Render.create({
-	    element: document.body,
+	    element: $("#field"),
 	    engine: engine
 	});
 
 	// create two boxes and a ground
-	var boxA = Bodies.rectangle(400, 200, 80, 80);
-	var boxB = Bodies.rectangle(450, 50, 80, 80);
+// 	var boxA = Bodies.rectangle(400, 200, 80, 80);
+// 	var boxB = Bodies.rectangle(450, 50, 80, 80);
+	var lPaddle = $("#leftPaddle");
+	var rPaddle = $("#rightPaddle");
 	var ground = Bodies.rectangle(400, 610, 810, 60, { isStatic: true });
 
 	// add all of the bodies to the world
